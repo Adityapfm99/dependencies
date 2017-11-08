81e2d30ab48e999015c850c4db05401d2b92e593
andreanw96 <andreanw96@gmail.com>
2017-11-08 14:39:13 +0700

    Tambahan account analytic

--- ds_api_analytic_account/models/analytic.py
+++ ds_api_analytic_account/models/analytic.py
@@ -7,12 +7,15 @@ class account_analytic_account(osv.osv):
 		"active": fields.boolean("Active"),
 		"alias_name": fields.char("Alias Name"),
 		"rds_id"	: fields.integer("ID RDS"),
-		"tag"	: fields.selection([('provinsi',"Provinsi"),
+		"tag"	: fields.selection([('head_office',"Head Office"),
+			('provinsi',"Provinsi"),
 			('kota',"Kabupaten/Kota"),
 			('gerai',"Gerai"),
 			('toko','Toko'),
 			('cabang','Cabang'),
 			('transit','Transit'),
+			('agen','Agen'),
+			('pusat_transitan','Pusat Transitan'),
 			('perwakilan',"Perwakilan"),
 			('other',"Lainnya")],"Category")
 		# "perwakilan_ids": fields.many2many("account.analytic.perwakilan","analytic_account_perwakilan_rel")

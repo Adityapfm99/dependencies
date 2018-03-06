a1dc7217483bdf33acde2fbafd266af5bf144583
Pambudi Satria <udi_blackf@yahoo.com>
2018-03-07 06:55:00 +0700

    [mail_track_readable_many2many]: add new modules to patch mail.thread

---
+++ mail_track_readable_many2many/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Mail Track Human Readable Many2many',
+    'category': 'Social Network',
+    'description': """
+Mail Track Human Readable Many2many
+===================================
+Fixing tracking on chatter for many2many field.
+""",
+    'author': 'Pambudi Satria',
+    'website': 'https://github.com/pambudisatria',
+    'depends': [
+        'mail',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

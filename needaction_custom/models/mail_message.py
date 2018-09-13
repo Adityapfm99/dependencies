c35ee77788b24691be63a20d5bcb53e69c7bc495
Pambudi Satria <udi_blackf@yahoo.com>
2018-09-13 14:22:30 +0700

    return without super

--- needaction_custom/models/mail_message.py
+++ needaction_custom/models/mail_message.py
@@ -1,4 +1,4 @@
-from openerp.osv import osv,fields
+from openerp.osv import osv, fields
 
 class mail_message(osv.Model):
 	""" Messages model: system notification (replacing res.log notifications),
@@ -6,10 +6,7 @@ class mail_message(osv.Model):
 	_inherit = 'mail.message'
 
 	def _needaction_count(self, cr, uid, domain=None, context=None):
-		res = super(mail_message,self)._needaction_count(cr, uid, context=context)
 		return 0
 
 	def _needaction_domain_get(self, cr, uid, context=None):
-		res = super(mail_message,self)._needaction_domain_get(cr, uid, context=context)
-
 		return False
